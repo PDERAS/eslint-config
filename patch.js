7df9fcb362e04203fab52dd3e668fb6b9140c0e1
Drew Bindon <drew.bindon@pderas.com>
2022-02-22 13:51:38 -0700

    Fix pathing

--- patch.js
+++ patch.js
@@ -1,4 +1,4 @@
 // ESLint doesn't follow sharable-configs 'extends' properly, here is a patch to enable this
 // https://github.com/eslint/eslint/issues/3458#issuecomment-516716165
 
-require("@rushstack/eslint-patch/modern-module-resolution");
+require("@rushstack/eslint-config/patch/modern-module-resolution");
