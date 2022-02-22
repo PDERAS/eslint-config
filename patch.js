7f586fcd8766b6ed095b13550446b34c72814289
Drew Bindon <drew.bindon@pderas.com>
2022-02-22 13:56:33 -0700

    Add the module export

--- patch.js
+++ patch.js
@@ -1,4 +1,14 @@
 // ESLint doesn't follow sharable-configs 'extends' properly, here is a patch to enable this
 // https://github.com/eslint/eslint/issues/3458#issuecomment-516716165
 
-require("@rushstack/eslint-config/patch/modern-module-resolution");
+require('@rushstack/eslint-config/patch/modern-module-resolution');
+
+module.exports = {
+	plugins: ['@rushstack/eslint-config'],
+	extends: [
+		"plugin:@rushstack/eslint-config/profile/node",
+		"plugin:@rushstack/eslint-config/mixins/packlets"
+		// "@rushstack/eslint-config/mixins/friendly-locals" // TODO: activate this rule
+	],
+	parserOptions: { tsconfigRootDir: __dirname }
+};
